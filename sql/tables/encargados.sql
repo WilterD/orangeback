f15d13f699c2acc1aa17f744f223e67588c65b13
WilterD <wilterd18@gmail.com>
2023-06-28 15:56:42 -0400

    modf encargados

--- sql/tables/encargados.sql
+++ sql/tables/encargados.sql
@@ -1,8 +1,10 @@
 CREATE TABLE encargados (
 cedula_encargado VARCHAR(15) PRIMARY KEY,
 nombre VARCHAR(50) NOT NULL,
-telefono VARCHAR(15),
-correo VARCHAR(50),
-id_orden INT,
-FOREIGN KEY (id_orden) REFERENCES Orden(id_orden) ON DELETE CASCADE ON UPDATE CASCADE
+telefono_principal VARCHAR(15),
+telefono_secundario VARCHAR(15),
+direccion_encargado VARCHAR(50),
+correo_encargado VARCHAR(50),
+id_cargo INT,
+FOREIGN KEY (id_cargo) REFERENCES cargos(id_cargo) ON DELETE CASCADE ON UPDATE CASCADE
 );
