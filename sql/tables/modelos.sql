e7b1bb4b703f32474823ec7ab4c85b563b1b8a91
WilterD <wilterd18@gmail.com>
2023-06-28 16:02:26 -0400

    modf modelo

--- sql/tables/modelos.sql
+++ sql/tables/modelos.sql
@@ -1,13 +1,12 @@
 CREATE TABLE modelos (
 codigo_modelo VARCHAR(50) NOT NULL PRIMARY KEY,
+marca VARCHAR(50) NOT NULL,
 nombre VARCHAR(50) NOT NULL,
-octanaje INT NOT NULL,
-aceite_caja VARCHAR(50) NOT NULL,
-id_marca INT NOT NULL,
-tipo_refrigerante VARCHAR(50) NOT NULL,
-tipo_aceite_motor VARCHAR(50) NOT NULL,
-cantidad_puestos INT NOT NULL,
-anio INT NOT NULL,
 peso INT NOT NULL,
-FOREIGN KEY (id_marca) REFERENCES marcas(id_marca)
+anio INT NOT NULL,
+tipo_refrigerante VARCHAR(50) NOT NULL,
+cantidad_puestos INT NOT NULL,
+tipo_aceite_motor VARCHAR(50) NOT NULL,
+aceite_caja VARCHAR(50) NOT NULL,
+octanaje INT NOT NULL
 );
