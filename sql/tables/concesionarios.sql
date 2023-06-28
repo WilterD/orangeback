4d55bbc2f70e12f1806245de02f1905e54391ce9
WilterD <wilterd18@gmail.com>
2023-06-28 15:51:44 -0400

    modificando concesionarios

--- sql/tables/concesionarios.sql
+++ sql/tables/concesionarios.sql
@@ -4,8 +4,8 @@ razon_social VARCHAR(50) NOT NULL,
 nombre_concesionario VARCHAR(50) NOT NULL,
 id_ciudad int NOT NULL,
 id_estado int NOT NULL,
-id_encargado INT NOT NULL,
-FOREIGN KEY (id_encargado) REFERENCES Personal(id_personal) ON DELETE CASCADE ON UPDATE CASCADE,
+cedula_encargado VARCHAR(15) NOT NULL,
+FOREIGN KEY (cedula_encargado) REFERENCES encargados(cedula_encargado) ON DELETE CASCADE ON UPDATE CASCADE,
 FOREIGN KEY (id_ciudad) REFERENCES ciudades(id_ciudad) ON DELETE CASCADE ON UPDATE CASCADE,
 FOREIGN KEY (id_estado) REFERENCES estados(id_estado) ON DELETE CASCADE ON UPDATE CASCADE
 );
