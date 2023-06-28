a6cc6e24ddc5d560260cba26ca35eb65eabcb83f
WilterD <wilterd18@gmail.com>
2023-06-28 16:05:09 -0400

    modf vehiculos llaves foraneas

--- sql/tables/vehiculos.sql
+++ sql/tables/vehiculos.sql
@@ -8,5 +8,11 @@ descripcion_extra VARCHAR(100) NOT NULL,
 resumen_mantenimiento VARCHAR(100) NOT NULL,
 concesionario_vendedor VARCHAR(100) NOT NULL,
 codigo_modelo VARCHAR(50) NOT NULL,
-cedula_cliente VARCHAR(15) NOT NULL
+cedula_cliente VARCHAR(15) NOT NULL,
+
+CONSTRAINT fk_vehiculos_modelos
+FOREIGN KEY (codigo_modelo) REFERENCES modelos(codigo_modelo),
+
+CONSTRAINT fk_vehiculos_clientes
+FOREIGN KEY (cedula_cliente) REFERENCES clientes(cedula_cliente)
 );
