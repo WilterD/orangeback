cbf28a6f3e2d42c6f837d078cf2c703d540e3cda
WilterD <wilterd18@gmail.com>
2023-06-28 16:07:09 -0400

    modf clientes

--- sql/tables/clientes.sql
+++ sql/tables/clientes.sql
@@ -3,6 +3,5 @@ cedula_cliente VARCHAR(15) PRIMARY KEY,
 nombre_cliente VARCHAR(50) NOT NULL,
 telefono_principal VARCHAR(15),
 telefono_secundario VARCHAR(15),
-direccion VARCHAR(100),
 correo_cliente VARCHAR(50)
 );
