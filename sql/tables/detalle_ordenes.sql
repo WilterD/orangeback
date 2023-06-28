7339d31986e49c53a2d4eb2ad7a2ff6fb56c35cd
WilterD <wilterd18@gmail.com>
2023-06-28 15:10:45 -0400

    tabla detalle ordenes

---
+++ sql/tables/detalle_ordenes.sql
@@ -0,0 +1,10 @@
+CREATE TABLE detalle_ordenes (
+id_detalle_orden INT PRIMARY KEY,
+id_orden INT,
+id_actividad INT,
+precio FLOAT,
+horas_tomadas INT,
+
+FOREIGN KEY (id_orden) REFERENCES ordenes(id_orden),
+FOREIGN KEY (id_actividad) REFERENCES actividades(id_actividad)
+);
