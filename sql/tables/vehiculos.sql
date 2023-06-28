b6b27ad73ea4a8d5187237ad9c118ca18edcd713
WilterD <wilterd18@gmail.com>
2023-06-28 16:04:20 -0400

    modf behiculos

--- sql/tables/vehiculos.sql
+++ sql/tables/vehiculos.sql
@@ -1,10 +1,12 @@
 CREATE TABLE vehiculos (
 placa VARCHAR(10) NOT NULL PRIMARY KEY,
-nro_motor VARCHAR(50),
 nro_serial VARCHAR(50),
+nro_motor VARCHAR(50),
 fecha_venta DATE,
 color VARCHAR(20),
 descripcion_extra VARCHAR(100) NOT NULL,
 resumen_mantenimiento VARCHAR(100) NOT NULL,
-concesionario_vendedor VARCHAR(100) NOT NULL
+concesionario_vendedor VARCHAR(100) NOT NULL,
+codigo_modelo VARCHAR(50) NOT NULL,
+cedula_cliente VARCHAR(15) NOT NULL
 );
