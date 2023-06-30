4e0fe374177a53536826a591673db7d68c96ef53
WilterD <wilterd18@gmail.com>
2023-06-29 22:38:00 -0400

    modf concesionarios

--- sql/tables/concesionarios.sql
+++ sql/tables/concesionarios.sql
@@ -3,9 +3,7 @@ rif_concesionario VARCHAR(20) NOT NULL PRIMARY KEY,
 razon_social VARCHAR(50) NOT NULL,
 nombre_concesionario VARCHAR(50) NOT NULL,
 id_city int NOT NULL,
-id_state int NOT NULL,
 dni_encargado VARCHAR(15) NOT NULL,
 FOREIGN KEY (dni_encargado) REFERENCES encargados(dni_encargado) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (id_city) REFERENCES cities(id_city) ON DELETE CASCADE ON UPDATE CASCADE,
-FOREIGN KEY (id_state) REFERENCES state(id_state) ON DELETE CASCADE ON UPDATE CASCADE
+FOREIGN KEY (id_city) REFERENCES cities(id_city) ON DELETE CASCADE ON UPDATE CASCADE
 );
