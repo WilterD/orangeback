f12c5f4631b98b0686b3610118b938975d5d6b2f
WilterD <wilterd18@gmail.com>
2023-06-29 23:47:49 -0400

    reserves

--- sql/tables/reserves.sql
+++ sql/tables/reserves.sql
@@ -1,10 +1,10 @@
-CREATE TABLE reservas (
+CREATE TABLE reserves (
 id_reserve INT NOT NULL,
-fecha_exped DATE NOT NULL,
-fecha_expir DATE NOT NULL,
+date_exped DATE NOT NULL,
+date_expir DATE NOT NULL,
 dni_client INT NOT NULL,
 id_vehiculo INT NOT NULL,
-hora TIME NOT NULL,
+hour TIME NOT NULL,
 
 PRIMARY KEY (id_reserve),
 
