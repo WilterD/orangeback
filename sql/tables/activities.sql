08df50bf956c218fe9fd0a5fa719a170f8193e0b
WilterD <wilterd18@gmail.com>
2023-06-30 00:03:06 -0400

    activities fix error

--- sql/tables/activities.sql
+++ sql/tables/activities.sql
@@ -4,5 +4,5 @@ id_activity INT,
 cost FLOAT,
 descriptions VARCHAR(200),
 PRIMARY KEY (id_service, id_activity),
-CONSTRAINT fk_activities_services FOREIGN KEY (id_service) REFERENCES services(id_service) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT fk_activities_services FOREIGN KEY (id_service) REFERENCES services(id_service) ON DELETE CASCADE ON UPDATE CASCADE
 );
