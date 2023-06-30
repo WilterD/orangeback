2f3b00d1f480a0b3ffbe8d7b4c6b3dbedde9604d
WilterD <wilterd18@gmail.com>
2023-06-29 21:26:02 -0400

    correccion en facturas

--- sql/tables/facturas.sql
+++ sql/tables/facturas.sql
@@ -3,8 +3,6 @@ id_factura INT PRIMARY KEY,
 fecha DATE,
 valor_descuento INT NOT NULL,
 monto_total DECIMAL(10, 2),
-id_cliente INT,
 id_orden INT,
-FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente) ON DELETE CASCADE ON UPDATE CASCADE,
 FOREIGN KEY (id_orden) REFERENCES ordenes(id_orden) ON DELETE CASCADE ON UPDATE CASCADE
 );
