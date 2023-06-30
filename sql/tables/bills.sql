45407df5931c21e711d170329b90e83276f771e0
WilterD <wilterd18@gmail.com>
2023-06-29 23:07:45 -0400

    cambiando claves primarias disenos

--- sql/tables/bills.sql
+++ sql/tables/bills.sql
@@ -1,8 +1,11 @@
 CREATE TABLE bills (
-id_bill INT PRIMARY KEY,
+id_bill INT,
 date_bill DATE,
 discount_value INT NOT NULL,
 total_amount DECIMAL(10, 2),
 id_order INT,
+
+PRIMARY KEY (id_bill),
+
 FOREIGN KEY (id_order) REFERENCES orders(id_order) ON DELETE CASCADE ON UPDATE CASCADE
 );
