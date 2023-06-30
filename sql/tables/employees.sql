da24b64362862964d4178f78791f51b25382f8fc
WilterD <wilterd18@gmail.com>
2023-06-29 23:41:25 -0400

    employees

---
+++ sql/tables/employees.sql
@@ -0,0 +1,12 @@
+CREATE TABLE employees (
+dni_employees VARCHAR(15) NOT NULL,
+employee_name VARCHAR(50) NOT NULL,
+id_charge VARCHAR(50),
+tlf_employee VARCHAR(15),
+addres_employee VARCHAR(50),
+
+PRIMARY KEY (dni_employees),
+
+FOREIGN KEY (id_charge) REFERENCES charges(id_charge) ON DELETE CASCADE ON UPDATE CASCADE,
+
+);
