e24ac0a28e84407f7fa276ee3ebe23e574f6a36a
WilterD <wilterd18@gmail.com>
2023-06-29 22:56:38 -0400

    discounts

--- sql/tables/discounts.sql
+++ sql/tables/discounts.sql
@@ -1,8 +1,8 @@
-CREATE TABLE descuentos (
-id_descuento INT PRIMARY KEY,
+CREATE TABLE discounts (
+id_discount INT PRIMARY KEY,
 services_min INT,
 services_max INT,
-porcentaje FLOAT,
+percentages FLOAT,
 rif_agency VARCHAR(15),
 FOREIGN KEY (rif_agency) REFERENCES agency(rif_agency) ON DELETE CASCADE ON UPDATE CASCADE
 );
