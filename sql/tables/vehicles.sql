8d8cbb920f87fd207d6b2101d72112cf75bb4414
WilterD <wilterd18@gmail.com>
2023-06-29 23:52:47 -0400

    vehiculos english

--- sql/tables/vehicles.sql
+++ sql/tables/vehicles.sql
@@ -1,16 +1,16 @@
-CREATE TABLE vehiculos (
-placa VARCHAR(10) NOT NULL,
+CREATE TABLE vehicles (
+plate VARCHAR(10) NOT NULL,
 nro_serial VARCHAR(50) NOT NULL,
 nro_motor VARCHAR(50) NOT NULL,
-fecha_venta DATE NOT NULL,
+sale_date DATE NOT NULL,
 color VARCHAR(20) NOT NULL,
 descriptions_extra VARCHAR(100) NOT NULL,
-resumen_mantenimiento VARCHAR(100) NOT NULL,
-concesionario_vendedor VARCHAR(100) NOT NULL,
+summary_maintenance VARCHAR(100) NOT NULL,
+agency_seller VARCHAR(100) NOT NULL,
 id_model VARCHAR(50) NOT NULL,
 dni_client VARCHAR(15) NOT NULL,
 
-PRIMARY KEY (placa),
+PRIMARY KEY (plate),
 
 CONSTRAINT fk_vehiculos_models
 FOREIGN KEY (id_model) REFERENCES models(id_model),
