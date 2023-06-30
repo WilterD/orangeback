3e25069d40e273a17e7e6c4118e81510543f3bad
WilterD <wilterd18@gmail.com>
2023-06-29 21:46:57 -0400

    table coordinating_services

--- sql/tables/detalle_ordenes.sql
+++ sql/tables/detalle_ordenes.sql
@@ -1,11 +1,15 @@
 CREATE TABLE detalle_ordenes (
-id_detalle_orden INT PRIMARY KEY,
-id_orden INT,
-id_actividad INT,
-precio FLOAT,
-horas_tomadas INT,
+id_servicio INT NOT NULL,
+id_actividad INT NOT NULL,
+id_orden INT NOT NULL,
+precio FLOAT NOT NULL,
+horas_tomadas INT NOT NULL,
+cedula_trabajador VARCHAR(15) NOT NULL,
 
-FOREIGN KEY (id_orden) REFERENCES ordenes(id_orden),
-FOREIGN KEY (id_actividad) REFERENCES actividades(id_actividad),
-FOREIGN KEY (id_detalle_orden) REFERENCES ordenes(id_orden) ON DELETE CASCADE ON UPDATE CASCADE
+PRIMARY KEY (id_servicio, id_actividad, id_detalle_orden),
+
+CONSTRAINT fk_orden_detalles FOREIGN KEY (id_orden) REFERENCES ordenes(id_orden),
+CONSTRAINT fk_actividad_details_ordens FOREIGN KEY (id_actividad) REFERENCES actividades(id_actividad),
+CONSTRAINT fk_details_ordenes FOREIGN KEY (id_detalle_orden) REFERENCES ordenes(id_orden) ON DELETE CASCADE ON UPDATE CASCADE,
+CONSTRAINT fk_trabajador_details_ordens FOREIGN KEY (cedula_trabajador) REFERENCES trabajadores(cedula_trabajador)
 );
