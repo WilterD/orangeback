2efe028247b1bd58071ebd291aba8cfa3346134e
Alejandro <alejandrojrosas2001@gmail.com>
2023-06-30 22:20:15 -0400

    cities crud

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import express from 'express'
 
 import adminsRouter from './api/admins.routes'
 import statesRouter from './api/states.routes'
+import citiesRouter from './api/cities.routes'
 
 const router = express.Router()
 
 router.use('/admins', adminsRouter)
 router.use('/states', statesRouter)
+router.use('/cities', citiesRouter)
 
 export default router
