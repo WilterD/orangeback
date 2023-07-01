b3e2975ecf15f97fd5089f4a7cceb0ed7ba80769
Alejandro <alejandrojrosas2001@gmail.com>
2023-07-01 09:47:42 -0400

    a

--- src/routes/api/admins.routes.ts
+++ src/routes/api/admins.routes.ts
@@ -1,20 +1,20 @@
 import { Router } from 'express'
 import {
-  // getAdmins,
-  getAdminById
-  // addAdmin,
+  getAdmins,
+  getAdminById,
+  addAdmin
   // updateAdmin,
   // deleteAdmin
 } from '../../controllers/admins.controller'
-// import { schemaWard } from '../../middlewares/schemaWard'
-// import { adminsSchema } from '../../schemas/admins.schema'
+import { schemaWard } from '../../middlewares/schemaWard'
+import { adminsSchema } from '../../schemas/admins.schema'
 
 const router = Router()
 
 /* eslint-disable @typescript-eslint/no-misused-promises */
-// router.get('/', getAdmins)
+router.get('/', getAdmins)
 router.get('/:adminId', getAdminById)
-// router.post('/', schemaWard(adminsSchema), addAdmin)
+router.post('/', schemaWard(adminsSchema), addAdmin)
 // router.put('/:adminId', schemaWard(adminsSchema), updateAdmin)
 // router.delete('/:adminId', deleteAdmin)
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 
+import loginRouter from './api/login.routes'
 import adminsRouter from './api/admins.routes'
 import statesRouter from './api/states.routes'
 
 const router = express.Router()
 
+router.use('/login', loginRouter)
 router.use('/admins', adminsRouter)
 router.use('/states', statesRouter)
 
