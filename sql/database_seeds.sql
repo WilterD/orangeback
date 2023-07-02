dd2050163c65aa940bc0e3e0ad45aa656b68a787
wilterd <wilterd18@gmail.com>
2023-07-02 11:56:53 -0400

    seeds jobs

--- sql/database_seeds.sql
+++ sql/database_seeds.sql
@@ -11,6 +11,19 @@
 -- discounts
 
 -- jobs
+INSERT INTO jobs (description, created_at)
+VALUES 
+  ('Analista de seguridad', '2013-10-10 12:12:12'),
+  ('Desarrollador web', '2022-01-01 10:00:00'),
+  ('Analista de datos', '2022-02-15 14:30:00'),
+  ('Ingeniero de software', '2022-03-28 09:45:00'),
+  ('Especialista en marketing digital', '2022-04-10 13:15:00'),
+  ('Diseñador gráfico', '2022-05-05 11:00:00'),
+  ('Gestor de proyectos', '2022-06-20 15:30:00'),
+  ('Ingeniero de redes', '2022-07-12 12:00:00'),
+  ('Contador', '2022-08-08 09:00:00'),
+  ('Asistente administrativo', '2022-09-04 14:45:00'),
+  ('Especialista en recursos humanos', '2022-10-22 10:30:00');
 
 -- employees
 
