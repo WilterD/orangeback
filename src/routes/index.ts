a7830aeb0799d1a44a9b1282adb4dfeea295eeb6
wilterd <wilterd18@gmail.com>
2023-07-02 12:53:03 -0400

    eliminando agencies de crud jobs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import express from 'express'
 import adminsRouter from './api/admins.routes'
 import statesRouter from './api/states.routes'
 import managersRouter from './api/managers.routes'
-import agenciesRouter from './api/agencies.routes'
 import jobsRouter from './api/jobs.routes'
 
 const router = express.Router()
@@ -11,7 +10,6 @@ const router = express.Router()
 router.use('/admins', adminsRouter)
 router.use('/states', statesRouter)
 router.use('/managers', managersRouter)
-router.use('/agencies', agenciesRouter)
 router.use('/jobs', jobsRouter)
 
 export default router
