763085a196012444ead8d9e3dcb1edec423f3404
wilterd <wilterd18@gmail.com>
2023-07-02 20:15:22 -0400

    fix errors

--- src/controllers/employees.controller.ts
+++ src/controllers/employees.controller.ts
@@ -70,8 +70,26 @@ export const getEmployeeById = async (
 }
 
 const getEmployeesDataFromRequestBody = (req: Request): any[] => {
-  const { employee_dni, name, phone, address, salary, agency_rif, job_id, created_at} = req.body
-  const newEmployee = [employee_dni, name, phone, address, salary, agency_rif, job_id, created_at]
+  const { 
+    employee_dni, 
+    name, 
+    phone, 
+    address, 
+    salary, 
+    agency_rif, 
+    job_id, 
+    created_at
+  } = req.body
+  const newEmployee = [
+    employee_dni, 
+    name, 
+    phone, 
+    address, 
+    salary, 
+    agency_rif, 
+    job_id, 
+    created_at
+  ]
   return newEmployee
 }
 
@@ -98,17 +116,37 @@ export const addEmployee = async (
   }
 }
 
+const getEmployeesUpdateDataFromRequestBody = (req: Request): any[] => {
+  const { 
+    name, 
+    phone, 
+    address, 
+    salary, 
+    agency_rif, 
+    job_id
+    } = req.body;
+
+  const updateEmployee = [
+    name, 
+    phone, 
+    address, 
+    salary, 
+    agency_rif, 
+    job_id
+  ];
+  return updateEmployee;
+};
+
 export const updateEmployee = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
   try {
-    const updatedEmployee = getEmployeesDataFromRequestBody(req)
-    updatedEmployee.push(req.params.employeeId)
+    const updateEmployee = getEmployeesUpdateDataFromRequestBody(req)
+    updateEmployee.push(req.params.employeeId)
     const response = await pool.query({
-        // employee_dni, name, phone, address, salary, agency_rif, job_id, created_at
       text: 'UPDATE employees SET name = $1, phone = $2, address = $3, salary = $4, agency_rif = $5, job_id = 6$  WHERE employee_dni = $7',
-      values: updatedEmployee
+      values: updateEmployee
     })
     console.log(response)
     if (response.rowCount === 0) {
