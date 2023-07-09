69a9859edabd14e644b806e4e2163c8b0794e566
Alejandro <alejandrojrosas2001@gmail.com>
2023-07-09 16:36:40 -0400

    xd

--- src/controllers/services.controller/update.ts
+++ src/controllers/services.controller/update.ts
@@ -4,6 +4,7 @@ import { STATUS } from '../../utils/constants'
 import { handleControllerError } from '../../utils/responses/handleControllerError'
 import { ServiceData } from './interface'
 import { StatusError } from '../../utils/responses/status-error'
+import getServiceById from './getServiceById.util'
 
 const getServicesUpdateDataFromRequestBody = (
   req: Request
@@ -24,10 +25,9 @@ export const updateService = async (
 ): Promise<Response> => {
   try {
     const updatedService = getServicesUpdateDataFromRequestBody(req)
-    updatedService[0].push(req.params.serviceId)
     const responseService = await pool.query({
       text: 'UPDATE services SET description = $1 WHERE service_id = $2',
-      values: updatedService[0]
+      values: [...updatedService[0], req.params.serviceId]
     })
     if (responseService.rowCount === 0) {
       throw new StatusError({
@@ -36,16 +36,29 @@ export const updateService = async (
       })
     }
     for (let i = 0; i < updatedService[1].length; i++) {
-      updatedService[1][i].push(req.params.serviceId)
-      await pool.query({
-        text: 'UPDATE activities SET description = $1, cost_hour = $2 WHERE activity_id = $3 AND service_id = $4',
-        values: updatedService[1][i]
+      const { rows } = await pool.query({
+        text: 'SELECT COUNT(*) FROM activities WHERE service_id = $1 AND activity_id = $2',
+        values: [req.params.serviceId, updatedService[1][i][2]]
       })
+      if (Number(rows[0].count) === 0) {
+        await pool.query({
+          text: 'INSERT INTO activities (service_id, description, cost_hour) VALUES ($1, $2, $3)',
+          values: [req.params.serviceId, updatedService[1][i]]
+        })
+      }
     }
-    return res
-      .status(STATUS.OK)
-      .json({ message: 'Servicio modificado exitosamente' })
+    for (let i = 0; i < updatedService[1].length; i++) {
+      if (updatedService[1][i][2] !== null) {
+        await pool.query({
+          text: 'UPDATE activities SET description = $1, cost_hour = $2 WHERE activity_id = $3 AND service_id = $4',
+          values: [...updatedService[1][i], req.params.serviceId]
+        })
+      }
+    }
+    const service = await getServiceById(+req.params.serviceId)
+    return res.status(STATUS.OK).json(service)
   } catch (error: unknown) {
+    console.log(error)
     return handleControllerError(error, res)
   }
 }
