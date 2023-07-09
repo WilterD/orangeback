71d06e6bb74921bebfb789a3a5689e6cdc900942
Alejandro <alejandrojrosas2001@gmail.com>
2023-07-09 06:07:01 -0400

    seeds

--- sql/database_seeds.sql
+++ sql/database_seeds.sql
@@ -24,12 +24,6 @@ INSERT INTO states (
   ('Mérida'),
   ('Miranda');
 
-INSERT INTO states (
-  name,
-  created_at
-) VALUES
-  ('Bolívddar', '23-07-2023');
-
 -- cities
 
 INSERT INTO cities (
@@ -112,20 +106,19 @@ INSERT INTO discounts (
 -- jobs
 
 INSERT INTO jobs (
-  description, 
-  created_at
+  description
 ) VALUES 
-  ('Analista de seguridad', '2013-10-10 12:12:12'),
-  ('Desarrollador web', '2022-01-01 10:00:00'),
-  ('Analista de datos', '2022-02-15 14:30:00'),
-  ('Ingeniero de software', '2022-03-28 09:45:00'),
-  ('Especialista en marketing digital', '2022-04-10 13:15:00'),
-  ('Diseñador gráfico', '2022-05-05 11:00:00'),
-  ('Gestor de proyectos', '2022-06-20 15:30:00'),
-  ('Ingeniero de redes', '2022-07-12 12:00:00'),
-  ('Contador', '2022-08-08 09:00:00'),
-  ('Asistente administrativo', '2022-09-04 14:45:00'),
-  ('Especialista en recursos humanos', '2022-10-22 10:30:00');
+  ('Analista de seguridad'),
+  ('Desarrollador web'),
+  ('Analista de datos'),
+  ('Ingeniero de software'),
+  ('Especialista en marketing digital'),
+  ('Diseñador gráfico'),
+  ('Gestor de proyectos'),
+  ('Ingeniero de redes'),
+  ('Contador'),
+  ('Asistente administrativo'),
+  ('Especialista en recursos humanos');
 
 -- employees
 
