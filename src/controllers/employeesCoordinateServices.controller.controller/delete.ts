23fe01f674497ceaf416d64997d62842e632c422
wilterd <wilterd18@gmail.com>
2023-07-13 14:29:07 -0400

    no al chorizo employesCoordinatedServices

--- src/controllers/employeesCoordinateServices.controller.controller/delete.ts
+++ src/controllers/employeesCoordinateServices.controller.controller/delete.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from 'express'
-import { pool } from '../../database'
-import { STATUS } from '../../utils/constants'
-import { StatusError } from '../../utils/responses/status-error'
-import { handleControllerError } from '../../utils/responses/handleControllerError'
+import { Request, Response } from "express";
+import { pool } from "../../database";
+import { STATUS } from "../../utils/constants";
+import { StatusError } from "../../utils/responses/status-error";
+import { handleControllerError } from "../../utils/responses/handleControllerError";
 
 export const deleteEmployeeCoordinateService = async (
   req: Request,
@@ -10,17 +10,26 @@ export const deleteEmployeeCoordinateService = async (
 ): Promise<Response> => {
   try {
     const response = await pool.query({
-      text: 'DELETE FROM employees_coordinate_services WHERE employee_dni = $1 AND service_id = $2',
-      values: [req.params.employeeDni, req.params.serviceId]
-    })
+      text: `DELETE FROM 
+            employees_coordinate_services 
+          WHERE 
+            employee_dni = $1 
+            AND service_id = $2`,
+      values: [req.params.employeeDni, req.params.serviceId],
+    });
     if (response.rowCount === 0) {
       throw new StatusError({
-        message: 'No se pudo encontrar el registro solicitado',
-        statusCode: STATUS.NOT_FOUND
-      })
+        message: "No se pudo encontrar el registro solicitado",
+        statusCode: STATUS.NOT_FOUND,
+      });
     }
-    return res.status(STATUS.OK).json({ message: 'Coordinación de servicio Eliminada Exitosamente correctamente' })
+    return res
+      .status(STATUS.OK)
+      .json({
+        message:
+          "Coordinación de servicio Eliminada Exitosamente correctamente",
+      });
   } catch (error: unknown) {
-    return handleControllerError(error, res)
+    return handleControllerError(error, res);
   }
-}
+};
--- src/controllers/employeesCoordinateServices.controller.controller/update.ts
+++ src/controllers/employeesCoordinateServices.controller.controller/update.ts
@@ -1,42 +1,51 @@
-import { Request, Response } from 'express'
-import { pool } from '../../database'
-import { STATUS } from '../../utils/constants'
-import { handleControllerError } from '../../utils/responses/handleControllerError'
-import { StatusError } from '../../utils/responses/status-error'
+import { Request, Response } from "express";
+import { pool } from "../../database";
+import { STATUS } from "../../utils/constants";
+import { handleControllerError } from "../../utils/responses/handleControllerError";
+import { StatusError } from "../../utils/responses/status-error";
 
-const getEmployeeCoordinateServiceUpdateDataFromRequestBody = (req: Request): any[] => {
-  const {
-    reservationTime,
-    capacity
-  } = req.body
+const getEmployeeCoordinateServiceUpdateDataFromRequestBody = (
+  req: Request
+): any[] => {
+  const { reservationTime, capacity } = req.body;
 
-  const updatedEmployeeCoordinatesService = [
-    reservationTime,
-    capacity
-  ]
-  return updatedEmployeeCoordinatesService
-}
+  const updatedEmployeeCoordinatesService = [reservationTime, capacity];
+  return updatedEmployeeCoordinatesService;
+};
 
 export const updateEmployeeCoordinateService = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
   try {
-    const updatedEmployeeCoordinatesService = getEmployeeCoordinateServiceUpdateDataFromRequestBody(req)
-    updatedEmployeeCoordinatesService.push(req.params.employeeDni)
-    updatedEmployeeCoordinatesService.push(req.params.serviceId)
+    const updatedEmployeeCoordinatesService =
+      getEmployeeCoordinateServiceUpdateDataFromRequestBody(req);
+    updatedEmployeeCoordinatesService.push(req.params.employeeDni);
+    updatedEmployeeCoordinatesService.push(req.params.serviceId);
     const response = await pool.query({
-      text: 'UPDATE employees_coordinate_services SET reservation_time = $1, capacity = $2 WHERE employee_dni = $3 AND service_id = $4',
-      values: updatedEmployeeCoordinatesService
-    })
+      text: `UPDATE 
+                employees_coordinate_services 
+              SET 
+                reservation_time = $1, 
+                capacity = $2 
+              WHERE 
+                employee_dni = $3 
+                AND service_id = $4`,
+      values: updatedEmployeeCoordinatesService,
+    });
     if (response.rowCount === 0) {
       throw new StatusError({
-        message: 'No se pudo encontrar el registro',
-        statusCode: STATUS.NOT_FOUND
-      })
+        message: "No se pudo encontrar el registro",
+        statusCode: STATUS.NOT_FOUND,
+      });
     }
-    return res.status(STATUS.OK).json({ message: 'Servicio de coordinación de empleados modificado exitosamente' })
+    return res
+      .status(STATUS.OK)
+      .json({
+        message:
+          "Servicio de coordinación de empleados modificado exitosamente",
+      });
   } catch (error: unknown) {
-    return handleControllerError(error, res)
+    return handleControllerError(error, res);
   }
-}
+};
