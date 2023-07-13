c383dc24d7ba731540eb9a99eb57bf7445c0d9b7
wilterd <wilterd18@gmail.com>
2023-07-13 13:41:28 -0400

    no al chorizo clientes

--- src/controllers/clients.controller/delete.ts
+++ src/controllers/clients.controller/delete.ts
@@ -1,26 +1,33 @@
-import { Request, Response } from 'express'
-import { pool } from '../../database'
-import { STATUS } from '../../utils/constants'
-import { StatusError } from '../../utils/responses/status-error'
-import { handleControllerError } from '../../utils/responses/handleControllerError'
+import { Request, Response } from "express";
+import { pool } from "../../database";
+import { STATUS } from "../../utils/constants";
+import { StatusError } from "../../utils/responses/status-error";
+import { handleControllerError } from "../../utils/responses/handleControllerError";
 
 export const deleteClient = async (
-    req: Request,
-    res: Response
-  ): Promise<Response> => {
-    try {
-      const response = await pool.query({
-        text: 'DELETE FROM clients WHERE client_dni = $1',
-        values: [req.params.clientDni]
-      })
-      if (response.rowCount === 0) {
-        throw new StatusError({
-          message: `No se pudo encontrar el registro de id: ${req.params.clientDni}`,
-          statusCode: STATUS.NOT_FOUND
-        })
-      }
-      return res.status(STATUS.OK).json({ message: 'Cliente Eliminado exitosamente' })
-    } catch (error: unknown) {
-      return handleControllerError(error, res)
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const response = await pool.query({
+      text: `SELECT 
+                * 
+              FROM 
+                clients 
+              WHERE 
+                client_dni = $1`,
+      values: [req.params.clientDni],
+    });
+    if (response.rowCount === 0) {
+      throw new StatusError({
+        message: `No se pudo encontrar el registro de id: ${req.params.clientDni}`,
+        statusCode: STATUS.NOT_FOUND,
+      });
     }
+    return res
+      .status(STATUS.OK)
+      .json({ message: "Cliente Eliminado exitosamente" });
+  } catch (error: unknown) {
+    return handleControllerError(error, res);
   }
+};
--- src/controllers/clients.controller/getAll.ts
+++ src/controllers/clients.controller/getAll.ts
@@ -1,19 +1,24 @@
-import { Request, Response } from 'express'
-import { pool } from '../../database'
-import camelizeObject from '../../utils/camelizeObject'
-import { handleControllerError } from '../../utils/responses/handleControllerError'
-import { STATUS } from '../../utils/constants'
+import { Request, Response } from "express";
+import { pool } from "../../database";
+import camelizeObject from "../../utils/camelizeObject";
+import { handleControllerError } from "../../utils/responses/handleControllerError";
+import { STATUS } from "../../utils/constants";
 
 export const getAllClients = async (
-    _: Request,
-    res: Response
-  ): Promise<Response> => {
-    try {
-      const { rows } = await pool.query({
-        text: 'SELECT * FROM clients ORDER BY name'
-      })
-      return res.status(STATUS.OK).json(camelizeObject(rows))
-    } catch (error: unknown) {
-      return handleControllerError(error, res)
-    }
+  _: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { rows } = await pool.query({
+      text: `SELECT 
+                * 
+              FROM 
+                clients 
+              ORDER BY 
+                name`,
+    });
+    return res.status(STATUS.OK).json(camelizeObject(rows));
+  } catch (error: unknown) {
+    return handleControllerError(error, res);
   }
+};
