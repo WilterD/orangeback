0a6703d2e8f2be8579e519adaeb11db1091eb58c
wilterd <wilterd18@gmail.com>
2023-07-13 14:50:55 -0400

    no al chorizo cities

--- src/controllers/cities.controller/update.ts
+++ src/controllers/cities.controller/update.ts
@@ -1,29 +1,34 @@
-import { Request, Response } from 'express'
-import { pool } from '../../database'
-import { STATUS } from '../../utils/constants'
-import { handleControllerError } from '../../utils/responses/handleControllerError'
-import { StatusError } from '../../utils/responses/status-error'
-import { getCitiesDataFromRequestBody } from './add'
+import { Request, Response } from "express";
+import { pool } from "../../database";
+import { STATUS } from "../../utils/constants";
+import { handleControllerError } from "../../utils/responses/handleControllerError";
+import { StatusError } from "../../utils/responses/status-error";
+import { getCitiesDataFromRequestBody } from "./add";
 
 export const updateCity = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
   try {
-    const updatedCity = getCitiesDataFromRequestBody(req)
-    updatedCity.push(req.params.cityId)
+    const updatedCity = getCitiesDataFromRequestBody(req);
+    updatedCity.push(req.params.cityId);
     const response = await pool.query({
-      text: 'UPDATE cities SET name = $1, state_id = $2 WHERE city_id = $3',
-      values: updatedCity
-    })
+      text: `UPDATE cities 
+      SET name = $1, 
+      state_id = $2
+      WHERE city_id = $3`,
+      values: updatedCity,
+    });
     if (response.rowCount === 0) {
       throw new StatusError({
         message: `No se pudo encontrar el registro de id: ${req.params.cityId}`,
-        statusCode: STATUS.NOT_FOUND
-      })
+        statusCode: STATUS.NOT_FOUND,
+      });
     }
-    return res.status(STATUS.OK).json({ message: 'Ciudad modificada exitosamente' })
+    return res
+      .status(STATUS.OK)
+      .json({ message: "Ciudad modificada exitosamente" });
   } catch (error: unknown) {
-    return handleControllerError(error, res)
+    return handleControllerError(error, res);
   }
-}
+};
