febd98c7add79b17cb6f41ae452a69c7034df07a
Alejandro <alejandrojrosas2001@gmail.com>
2023-07-15 17:02:24 -0400

    orders done

---
+++ src/controllers/orders.controller/updateOrder.action.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from 'express'
+import { pool } from '../../database'
+import { STATUS } from '../../utils/constants'
+import { StatusError } from '../../utils/responses/status-error'
+import { handleControllerError } from '../../utils/responses/handleControllerError'
+import { getOrdersDataFromRequestBody } from './addOrder.action'
+
+export default async function updateOrder (
+  req: Request,
+  res: Response
+): Promise<Response> {
+  try {
+    const updatedOrder = getOrdersDataFromRequestBody(req)
+    const response = await pool.query({
+      text: `
+        UPDATE orders SET 
+          entry_time = $1, 
+          estimated_departure = $2, 
+          booking_id = $3, 
+          employee_dni = $4, 
+          real_departure = $5, 
+          responsible_dni = $6, 
+          responsible_name = $7
+        WHERE order_id = $8
+      `,
+      values: [...updatedOrder, req.params.orderId]
+    })
+    if (response.rowCount === 0) {
+      throw new StatusError({
+        message: `No se pudo encontrar el registro de id: ${req.params.managerDni}`,
+        statusCode: STATUS.NOT_FOUND
+      })
+    }
+    return res.status(STATUS.OK).json({ message: 'Orden modificada exitosamente' })
+  } catch (error: unknown) {
+    console.log(error)
+    return handleControllerError(error, res)
+  }
+}
--- src/schemas/orders.schema.ts
+++ src/schemas/orders.schema.ts
@@ -1,18 +1,20 @@
 import { z } from 'zod'
 
-export const createOrdersSchema = z.object({
-  responsible_dni: z
+export const ordersSchema = z.object({
+  responsibleDni: z
     .string()
     .nonempty('Es necesario indicar una cédula')
     .max(16, 'La cédula debe ser menor a 16 caracteres')
     .regex(/^\d+$/, 'La cédula debe contener solo números')
-    .optional(),
-  responsible_name: z
+    .optional()
+    .nullable(),
+  responsibleName: z
     .string()
     .nonempty('Es necesario indicar un nombre de encargado')
     .max(64, 'El nombre debe ser menor a 64 caracteres')
-    .optional(),
-  entry_time: z
+    .optional()
+    .nullable(),
+  entryTime: z
     .string()
     .nonempty('Debe especificar una fecha y tiempo de entrada del vehículo')
     .refine(
@@ -32,7 +34,7 @@ export const createOrdersSchema = z.object({
         message: 'La fecha debe estar en formato DD-MM-AAAA HH:MM:SS y ser una fecha válida y dentro de los límites permitidos'
       }
     ),
-  estimated_departure: z
+  estimatedDeparture: z
     .string()
     .nonempty('Debe especificar una fecha y tiempo estimado de salida para el vehículo')
     .refine(
@@ -52,7 +54,7 @@ export const createOrdersSchema = z.object({
         message: 'La fecha debe estar en formato DD-MM-AAAA HH:MM:SS y ser una fecha válida y dentro de los límites permitidos'
       }
     ),
-  real_departure: z
+  realDeparture: z
     .string()
     .nonempty('Debe especificar una fecha y tiempo de salida real del vehículo')
     .refine(
@@ -72,91 +74,15 @@ export const createOrdersSchema = z.object({
         message: 'La fecha debe estar en formato DD-MM-AAAA HH:MM:SS y ser una fecha válida y dentro de los límites permitidos'
       }
     )
-    .optional(),
-  booking_id: z
+    .optional()
+    .nullable(),
+  bookingId: z
     .number(),
-  employee_dni: z
+  employeeDni: z
     .string()
     .nonempty('Es necesario indicar una cédula')
     .max(16, 'La cédula debe ser menor a 16 caracteres')
     .regex(/^\d+$/, 'La cédula debe contener solo números')
 })
 
-export const updateOrdersSchema = z.object({
-  responsible_dni: z
-    .string()
-    .nonempty('Es necesario indicar una cédula')
-    .max(16, 'La cédula debe ser menor a 16 caracteres')
-    .regex(/^\d+$/, 'La cédula debe contener solo números'),
-  responsible_name: z
-    .string()
-    .nonempty('Es necesario indicar un nombre de encargado')
-    .max(64, 'El nombre debe ser menor a 64 caracteres'),
-  entry_time: z
-    .string()
-    .nonempty('Debe especificar una fecha y tiempo de entrada del vehículo')
-    .refine(
-      (fecha) => {
-        const regex = /^(\d{2})-(\d{2})-(\d{4})\s+(\d{2}):(\d{2}):(\d{2})$/
-        if (!regex.test(fecha)) {
-          return false
-        }
-        const [dia, mes, anio, hora, minutos, segundos] = fecha.split(/[-\s:]/).map(Number)
-        if (dia < 1 || dia > 30 || mes < 1 || mes > 12 || anio < 1900 || anio > 2100 ||
-          hora < 0 || hora > 23 || minutos < 0 || minutos > 59 || segundos < 0 || segundos > 59) {
-          return false
-        }
-        return true
-      },
-      {
-        message: 'La fecha debe estar en formato DD-MM-AAAA HH:MM:SS y ser una fecha válida y dentro de los límites permitidos'
-      }
-    ),
-  estimated_departure: z
-    .string()
-    .nonempty('Debe especificar una fecha y tiempo estimado de salida para el vehículo')
-    .refine(
-      (fecha) => {
-        const regex = /^(\d{2})-(\d{2})-(\d{4})\s+(\d{2}):(\d{2}):(\d{2})$/
-        if (!regex.test(fecha)) {
-          return false
-        }
-        const [dia, mes, anio, hora, minutos, segundos] = fecha.split(/[-\s:]/).map(Number)
-        if (dia < 1 || dia > 30 || mes < 1 || mes > 12 || anio < 1900 || anio > 2100 ||
-          hora < 0 || hora > 23 || minutos < 0 || minutos > 59 || segundos < 0 || segundos > 59) {
-          return false
-        }
-        return true
-      },
-      {
-        message: 'La fecha debe estar en formato DD-MM-AAAA HH:MM:SS y ser una fecha válida y dentro de los límites permitidos'
-      }
-    ),
-  real_departure: z
-    .string()
-    .nonempty('Debe especificar una fecha y tiempo de salida real del vehículo')
-    .refine(
-      (fecha) => {
-        const regex = /^(\d{2})-(\d{2})-(\d{4})\s+(\d{2}):(\d{2}):(\d{2})$/
-        if (!regex.test(fecha)) {
-          return false
-        }
-        const [dia, mes, anio, hora, minutos, segundos] = fecha.split(/[-\s:]/).map(Number)
-        if (dia < 1 || dia > 30 || mes < 1 || mes > 12 || anio < 1900 || anio > 2100 ||
-          hora < 0 || hora > 23 || minutos < 0 || minutos > 59 || segundos < 0 || segundos > 59) {
-          return false
-        }
-        return true
-      },
-      {
-        message: 'La fecha debe estar en formato DD-MM-AAAA HH:MM:SS y ser una fecha válida y dentro de los límites permitidos'
-      }
-    )
-    .optional(),
-  booking_id: z.number(),
-  employee_dni: z
-    .string()
-    .nonempty('Es necesario indicar una cédula')
-    .max(16, 'La cédula debe ser menor a 16 caracteres')
-    .regex(/^\d+$/, 'La cédula debe contener solo números')
-})
+export type OrderCreate = z.infer<typeof ordersSchema>
