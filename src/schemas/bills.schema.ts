ed088f636b4350c3fba7860ade92f36dec5e69ad
Alejandro <alejandrojrosas2001@gmail.com>
2023-07-15 09:50:56 -0400

    mejoras a los mensajes de error de esquemas

--- src/schemas/bills.schema.ts
+++ src/schemas/bills.schema.ts
@@ -1,39 +1,28 @@
 import { z } from 'zod'
 
-export const createBillsSchema = z.object({
-    billDate: z
-    .string()
-    .refine(
-      (fecha) => {
-        const regex = /^(\d{4})-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01])$/
-        return regex.test(fecha)
-      },
-      {
-        message:
-        'La fecha debe estar en formato DD-MM-AAAA y ser una fecha válida'
-      }
-    ),
-    discountValue: z
-    .number(),
-    orderId: z
-    .number()
-})
-
-export const updateBillsSchema = z.object({
+export const billsSchema = z.object({
   billDate: z
     .string()
+    .nonempty('Debe especificar una fecha para la factura')
     .refine(
       (fecha) => {
-        const regex = /^(\d{4})-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01])$/
-        return regex.test(fecha)
+        const regex = /^(\d{2})-(\d{2})-(\d{4})\s+(\d{2}):(\d{2}):(\d{2})$/
+        if (!regex.test(fecha)) {
+          return false
+        }
+        const [dia, mes, anio, hora, minutos, segundos] = fecha.split(/[-\s:]/).map(Number)
+        if (dia < 1 || dia > 30 || mes < 1 || mes > 12 || anio < 1900 || anio > 2100 ||
+        hora < 0 || hora > 23 || minutos < 0 || minutos > 59 || segundos < 0 || segundos > 59) {
+          return false
+        }
+        return true
       },
       {
-        message:
-        'La fecha debe estar en formato DD-MM-AAAA y ser una fecha válida'
+        message: 'La fecha debe estar en formato DD-MM-AAAA HH:MM:SS y ser una fecha válida y dentro de los límites permitidos'
       }
     ),
-    discountValue: z
+  discountValue: z
     .number(),
-    orderId: z
+  orderId: z
     .number()
 })
