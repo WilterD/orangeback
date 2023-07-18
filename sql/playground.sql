5e6a68790f961c8c88e3522e3b1b01594b34b064
Alejandro <alejandrojrosas2001@gmail.com>
2023-07-18 05:34:06 -0400

    cambio de ruta en bills

--- sql/playground.sql
+++ sql/playground.sql
@@ -17,12 +17,13 @@ WHERE
   o.order_id = 1 AND
   o.employee_dni = e.employee_dni AND
   e.agency_rif = d.agency_rif AND
-  (SELECT COALESCE(COUNT(o.order_id), 0) AS order_count
-FROM clients AS c
-LEFT JOIN bookings AS b ON c.client_dni = b.client_dni
-LEFT JOIN orders AS o ON b.booking_id = o.booking_id
-WHERE 
-  c.client_dni = '89012345' AND 
-  o.real_departure >= DATE_TRUNC('month', NOW() - INTERVAL '6 months')
-  ) IS BETWEEN ;
+  (
+    SELECT COALESCE(COUNT(o.order_id), 0) AS order_count
+    FROM clients AS c
+    LEFT JOIN bookings AS b ON c.client_dni = b.client_dni
+    LEFT JOIN orders AS o ON b.booking_id = o.booking_id
+    WHERE 
+      c.client_dni = '89012345' AND 
+      o.real_departure >= DATE_TRUNC('month', NOW() - INTERVAL '6 months')
+  ) BETWEEN ;
   
