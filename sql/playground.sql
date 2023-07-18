01924217fc143f7d117686b02310362dfdf3e7e9
Alejandro <alejandrojrosas2001@gmail.com>
2023-07-18 04:56:54 -0400

    get all bills

--- sql/playground.sql
+++ sql/playground.sql
@@ -1,3 +1,4 @@
+-- Cantidad de Ordenes de un cliente dado en los últimos 6 meses
 SELECT COALESCE(COUNT(o.order_id), 0) AS order_count
 FROM clients AS c
 LEFT JOIN bookings AS b ON c.client_dni = b.client_dni
@@ -5,3 +6,15 @@ LEFT JOIN orders AS o ON b.booking_id = o.booking_id
 WHERE 
   c.client_dni = '89012345' AND 
   o.real_departure >= DATE_TRUNC('month', NOW() - INTERVAL '6 months');
+
+SELECT
+  d.percentage
+FROM
+  orders AS o,
+  employees AS e,
+  discounts AS d
+WHERE
+  o.order_id = 1 AND
+  o.employee_dni = e.employee_dni AND
+  e.agency_rif = d.agency_rif;
+  
---
+++ src/controllers/bills.controller/getAll.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express'
+import { pool } from '../../database'
+import { STATUS } from '../../utils/constants'
+import { handleControllerError } from '../../utils/responses/handleControllerError'
+import camelizeObject from '../../utils/camelizeObject'
+
+export const getAllBills = async (
+  _: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const { rows } = await pool.query({ text: 'SELECT * FROM bills' })
+    return res.status(STATUS.OK).json(camelizeObject(rows))
+  } catch (error: unknown) {
+    return handleControllerError(error, res)
+  }
+}
