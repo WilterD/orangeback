a71706bd6dbed20681fd265f377a90ff03fc477e
Feredev <hector1567xd@gmail.com>
2023-07-18 06:11:31 -0400

    Fixes getallservices

--- src/controllers/services.controller/getAllServices.action.ts
+++ src/controllers/services.controller/getAllServices.action.ts
@@ -6,19 +6,21 @@ import camelizeObject from '../../utils/camelizeObject'
 import { ServicePaginated } from './types'
 
 const getAllServices = async (
-  _: Request,
+  req: Request,
   res: Response
 ): Promise<Response> => {
   try {
-    const items = await executeGetAllServices()
+    const items = await executeGetAllServices(req)
     return res.status(STATUS.OK).json(items)
   } catch (error: unknown) {
     return handleControllerError(error, res)
   }
 }
 
-async function executeGetAllServices (): Promise<ServicePaginated[]> {
-  const text = `
+async function executeGetAllServices (
+  req: Request
+): Promise<ServicePaginated[]> {
+  let text = `
     SELECT
       s.service_id,
       s.description,
@@ -32,8 +34,57 @@ async function executeGetAllServices (): Promise<ServicePaginated[]> {
       s.created_at
     ORDER BY description`
 
-  const { rows } = await pool.query({ text })
-  return camelizeObject(rows) as unknown as ServicePaginated[]
+  let response
+
+  if (
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    req.query?.onlyForAgencyRif &&
+    req.query?.onlyForAgencyRif !== null &&
+    req.query?.onlyForAgencyRif !== 'null' &&
+    req.query?.onlyForAgencyRif !== ''
+  ) {
+    let discriminantServicesConditional = 'e.agency_rif = $1 AND'
+    const whereArgs = [req.query.onlyForAgencyRif]
+    if (
+      // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+      !!req.query?.includeServicesIds &&
+      req.query?.includeServicesIds !== null &&
+      req.query?.includeServicesIds !== 'null' &&
+      req.query?.includeServicesIds !== ''
+    ) {
+      discriminantServicesConditional = '(e.agency_rif = $1 OR s.service_id IN ($2)) AND'
+      whereArgs.push(req.query?.includeServicesIds)
+    }
+
+    text = `
+      SELECT
+        s.service_id,
+        s.description,
+        SUM(a.cost_hour) AS total_cost,
+        s.created_at
+      FROM
+        employees as e,
+        employees_coordinate_services AS ecs,
+        services AS s,
+        activities AS a
+      WHERE
+        ${discriminantServicesConditional}
+        e.employee_dni = ecs.employee_dni AND
+        ecs.service_id = s.service_id AND
+        s.service_id = a.service_id 
+      GROUP BY
+        s.service_id,
+        s.description,
+        s.created_at
+      ORDER BY description
+    `
+
+    response = await pool.query({ text, values: whereArgs })
+  } else {
+    response = await pool.query({ text })
+  }
+
+  return camelizeObject(response.rows) as unknown as ServicePaginated[]
 }
 
 export default getAllServices
